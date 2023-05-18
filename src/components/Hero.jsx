ec8ece40d2a66f82445ac44627d16e28e57d6f05
aleixnit <nitjac210@gmail.com>
2023-05-18 19:30:30 +0200

    hero section modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import "./Hero.css";
 import React from "react";
 import Typed from "typed.js";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 export default function Hero() {
   // Create reference to store the DOM element containing the animation
@@ -8,7 +9,16 @@ export default function Hero() {
 
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["Bienvenidos.", "Welcome.", "Benvinguts.", "Bem-vindo.", "Benvenuto.", "Willkommen."],
+      strings: [
+        "Bienvenidos.",
+        "Welcome.",
+        "Benvinguts.",
+        "Bem-vindo.",
+        "Benvenuto.",
+        "Willkommen.",
+        "喜ばしい.",
+      ],
+      loop: true,
       typeSpeed: 50,
     });
 
@@ -19,17 +29,16 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="px-5 mx-4 mt-5 rounded-3 bg-hero">
+    <div className="px-5 mx-4 mt-5 rounded-3 bg-hero d-flex">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold"><span ref={el} /></h1>
+        <h1 className="display-5 fw-bold">
+          <span ref={el} />
+        </h1>
         <p className="col-md-8 fs-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          aliquip ex ea commodo consequat.
         </p>
         <button className="learn-more">
           <span className="circle" aria-hidden="true">
@@ -38,35 +47,15 @@ export default function Hero() {
           <span className="button-text">Learn More</span>
         </button>
       </div>
+      <div className="p-1" >
+        <lottie-player
+          src="https://assets1.lottiefiles.com/packages/lf20_GbabwrUY2k.json"
+          style={{ width: "400px", height: "400px", background: "transparent" }}
+          speed="1"
+          loop
+          autoplay
+        ></lottie-player>
+      </div>
     </div>
-
-    // <div className="App">
-    //   <span ref={el} />
-    // </div>
   );
 }
-
-// export default function Hero() {
-//   return (
-//     <div className="px-5 mx-4 mt-5 rounded-3 bg-hero">
-//       <div className="container-fluid py-5">
-//         <h1 className="display-5 fw-bold">A warm welcome!👌</h1>
-//         <p className="col-md-8 fs-4">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//           aliquip ex ea commodo consequat. Duis aute irure dolor in
-//           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-//           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-//           culpa qui officia deserunt mollit anim id est laborum.
-//         </p>
-//         <button class="learn-more">
-//           <span class="circle" aria-hidden="true">
-//             <span class="icon arrow"></span>
-//           </span>
-//           <span class="button-text">Learn More</span>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
           id="navbarNavAltMarkup"
         >
           <div className="navbar-nav ">
-          <button className="btnMenu">
+            <button className="btnMenu">
               <span></span>
               <span></span>
               <span></span>
